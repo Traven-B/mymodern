b68d93bbb83c5da5adf38de0624562c4bc457bb5
Michael Kamb <michael.kamb@yahoo.com>
2025-09-10 01:28:45 -0500

    Remove many comment linse from secrets files
    Also have write to stderr from hennepin_secrets when no environment
    variables are set. A nag for new users. Env var not set, it's
    potentially a new user. Comment says to remove the code
    block. Better if only emitted when not using --mock, but that option is
    not visible.

--- src/hennepin_secrets.cr
+++ src/hennepin_secrets.cr
@@ -1,86 +1,54 @@
-## ##############################################
-##
-## Never commit this file with real credentials
-## to version control in a public repository.
-##
-## Don't commit this file with real credentials to your
-## local version control if you intend to share the code.
-##
-## ##############################################
-
-# This separate secrets file originally held my login information to prevent it
-# from being unintentionally published with the shared source code. It can now be
-# distributed as-is, as my credentials are currently accessed through environment
-# variables.
-
-# Using environment variables for credentials is convenient for me, although it
-# may not offer significant security benefits over other methods.
-
-# Library card credentials don't seem like top-secret information, so if you use
-# my code to scrape library web sites it seems ok to have your library card and
-# pin as clear text, even in the source code of the program.
-
-# If you use this model to store the pin to access another not really sensitive
-# website, like the one for tomorrows weather forecast, that seems ok too, but
-# I'm not telling you to use this code to scrape your banking information.
-
-# This file demonstrates two approaches for setting the `POST_DATA` constant:
-
-# 1. Direct String Assignment:
-# This approach embeds credentials directly in the code as a string literal.
-
-#    module MyTown  # Replace with your actual module name, e.g., MyLocalLibrary
-#      POST_DATA = "name=your-barcode-here&user_pin=your-secret-pin-here"
-#    end
-
-# 2. Environment Variable:
-# This approach attempts to retrieve credentials from an environment variable.
-# If the variable is not set, an exception is thrown.
-
-#    module MyTown
-#      POST_DATA = begin
-#        ENV["SECRET_MYTOWN"]
-#      rescue e : KeyError
-#        STDERR.puts e
-#        STDERR.puts "Error: Environment variable 'SECRET_MYTOWN' is not set."
-#        exit 1
-#      end
-#    end
-
-# I have an incomplete understanding of this, and am using an environment
-# variable for my convenience and additional security in my use case.
-
-#   AI bot says:
+# ##############################################
 #
-#   Reminder: Removing sensitive files from the latest commit (HEAD) does not
-#   purge them from the entire version control history. Use tools like
-#   'git filter-branch' or 'BFG Repo-Cleaner' to completely remove sensitive
-#   data from the repository's commit history before sharing the code.
-
-
-## ##############################################
-##
-## Never commit this file with real credentials
-## to version control in a public repository.
-##
-## Don't commit this file with real credentials to your
-## local version control if you intend to share the code.
-##
-## ##############################################
+# Never commit this file with real credentials
+# to version control in a public repository.
+#
+# Don't commit this file with real credentials to your
+# local version control if you intend to share the code.
+#
+# Even if you later stop hard coding the secrets or remove the secret files
+# from the latest version of the code using "git rm <file>" or similar
+# commands they will likely persist in the version control history, which would
+# allow someone to extract them. Just start a new repository if you later decide
+# to post the code would be a plan.
+#
+# ##############################################
 
 module Hennepin
-  # comment out one of the assignments to POST_DATA, use the other, if you
-  # comment out the assignment of a string literal it doesn't make sense to
-  # have real data there, replace with fake data.
+  # ## DELETE_ME
+  # ## extra extraneous introductory block of code.
+  # ## Remove the follwing begin  ..  end block.
+  begin
+    ENV["SECRET_HENNEPIN"]
+  rescue e : KeyError
+    STDERR.puts "Hello from hennepin_secrets.cr stderr."
+    STDERR.puts "When using this code and these fake credentials, " \
+                "run with --mock option to see full report with book titles."
+    STDERR.puts "Use --mock option when you are scraping your own sample web pages on your file system."
+    STDERR.puts "Don't use --mock when you specify URLs and secrets in modules for the website(s) you'll be scraping."
+    STDERR.puts "Use --mock option when testing your finished code."
+  end
+  # ## end DELETE_ME
 
-  # do this with your field names and values
-  # POST_DATA = "name=11111111111111&user_pin=2222"
-  # or do this
+  # always use --mock option until you develop youre own URL data and your own real secrets.
+
+  # Code to use for fake or real hard coded secrets. Unless you are supplingg the
+  # login string from an ENV variable. Do this if you don't want them hard coded.
+  #
+  # The take from the environment scheme is recommend in some contexts. It's good
+  # if you are going to share your code
+
   POST_DATA = begin
     ENV["SECRET_HENNEPIN"]
   rescue e : KeyError
-    STDERR.puts e
-    STDERR.puts "Error: Environment variable 'SECRET_HENNEPIN' is not set."
-    exit 1
+    # The string below is what is assigned to the constant when not using an environment
+    # variable to supply the login secret. Replace with your correctly formed real secret when
+    # you go to post to the login URL of the website you'll be scraping. This really
+    # should end up in a file of a different name containing the secrets for a module
+    # of a different name.
+    "name=fake_barcode&user_pin=fake_pin"
   end
+
+  # this is the result we want to achieve.
+  # POST_DATA = "name=11111111111111&user_pin=2222"
 end
--- src/stpaul_secrets.cr
+++ src/stpaul_secrets.cr
@@ -1,37 +1,24 @@
-# See hennepin_secrets.cr for my and the chat ai bots thoughts about managing not
-# so secret credentials.  How not to share them when sharing code anyway.
-# What you might actually consider doing if you were using actually important secrets I leave to you.
-
-#   AI bot says:
+# ##############################################
 #
-#   Reminder: Removing sensitive files from the latest commit (HEAD) does not
-#   purge them from the entire version control history. Use tools like
-#   'git filter-branch' or 'BFG Repo-Cleaner' to completely remove sensitive
-#   data from the repository's commit history before sharing the code.
-
-## ##############################################
-##
-## Never commit this file with real credentials
-## to version control in a public repository.
-##
-## Don't commit this file with real credentials to your
-## local version control if you intend to share the code.
-##
-## ##############################################
+# Never commit this file with real credentials
+# to version control in a public repository.
+#
+# Don't commit this file with real credentials to your
+# local version control if you intend to share the code.
+#
+# Even if you later stop hard coding the secrets or remove the secret files
+# from the latest version of the code using "git rm <file>" or similar
+# commands they will likely persist in the version control history, which would
+# allow someone to extract them. Just start a new repository if you later decide
+# to post the code would be a plan.
+#
+# ##############################################
 
 module StPaul
-  # comment out one of the assignments to POST_DATA, use the other, if you
-  # comment out the assignment of a string literal it doesn't make sense to
-  # have real data there, replace with fake data.
-
-  # do this with your field names and values
-  # POST_DATA = "code=11111111111111&pin=222222"
-  # or do this
   POST_DATA = begin
     ENV["SECRET_STPAUL"]
   rescue e : KeyError
-    STDERR.puts e
-    STDERR.puts "Error: Environment variable 'SECRET_STPAUL' is not set."
-    exit 1
+    # string below is what is assinged to the constant
+    "code=fake_code&pin=fake_pin"
   end
 end
