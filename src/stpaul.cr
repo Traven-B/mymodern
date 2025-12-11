541d0649c69b2280282ef8e17ce5d3513830485a
Michael Kamb <michael.kamb@yahoo.com>
2025-12-11 00:18:29 -0600

    Use St. Paul v2: Bibliocommons parity with Hennepin
    - Replace table selectors with div based parsing (identical to Hennepin)
    - Add s_c_v2.html, s_h_v2.html fixtures from new site
    - Remove obsolete s_c.html, s_h.html fixtures
    - Update mock_expected for v2 output (nearly same text)

--- src/stpaul.cr
+++ src/stpaul.cr
@@ -2,105 +2,95 @@ module StPaul
   def self.lib_data
     {
       post_data:           POST_DATA, # constant defined in stpaul_secrets.cr, but must be named here also
-      post_url:            STPAUL_BASE_URL + "/patroninfo~S16",
-      checked_out_url:     STPAUL_BASE_URL + "/patroninfo~S16/1600472/items",
-      holds_url:           STPAUL_BASE_URL + "/patroninfo~S16/1600472/holds",
+      post_url:            STPAUL_BASE_URL + "/user/login?%2Fuser_dashboard",
+      checked_out_url:     STPAUL_BASE_URL + "/v2/checkedout",
+      holds_url:           STPAUL_BASE_URL + "/v2/holds/ready_for_pickup",
       print_name:          "St. Paul",
-      checked_out_fixture: "s_c.html",
-      holds_fixture:       "s_h.html",
+      checked_out_fixture: "s_c_v2.html",
+      holds_fixture:       "s_h_v2.html",
       trace_name:          "StPaul",
     }
   end
 
   def self.parse_checkedout_page(page)
     doc = Lexbor::Parser.new(page)
-    books_out = doc.css("table tr.patFuncEntry").to_a.map do |book_part|
+    books_out = doc.css("div.cp-checked-out-item").to_a.map do |book_part|
       the_title = find_checkedout_title(book_part)
       the_date = find_checkedout_date(book_part)
       the_renewed_count = find_renewed_count(book_part)
-      the_number_waiting = 0
+      the_number_waiting = find_number_waiting(book_part)
       CheckedOutRecord.new(the_title, the_date, the_renewed_count, the_number_waiting)
     end
+    # ##### no never mind ##### => books_out.sort_by { |a| Recs.create_checked_composite_key(a) }
     books_out.sort { |a, b| Recs.checked_compare(a, b) }
   end
 
   def self.parse_on_hold_page(page)
-    # For on hold books, status might be <td class="patFuncStatus"> 16 of 19 holds </td>
-    # We want to select only those like <td class="patFuncStatus"> Ready. Must pick up by 10-25-16 </td>
-    # The if statement generates a record when status.match(/^Ready/) is true, otherwise producing nil.
-    # compact_map removes nils, then maps remaining non-nil elements by evaluating the block.
-    # This allows operating only on book_parts that matched /^Ready/ and not any spurious nils.
     doc = Lexbor::Parser.new(page)
-    books_on_hold = doc.css("table tr.patFuncEntry").to_a.compact_map do |book_part|
-      td_status = book_part.css("td.patFuncStatus").first
-      status = td_status.inner_text.strip
-      if status.match(/^Ready/)
-        the_title = find_on_hold_title(book_part)
-        the_date = find_on_hold_date(status)
-        OnHoldRecord.new(the_title, the_date)
-      end
+    books_on_hold = doc.css("div.cp-bib-list-item.cp-hold-item.ready_for_pickup").to_a.map do |book_part|
+      the_title = find_on_hold_title(book_part)
+      the_date = find_on_hold_date(book_part)
+      OnHoldRecord.new(the_title, the_date)
     end
+    # ##### no never mind ##### => books_on_hold.sort_by { |a| Recs.create_holds_composite_key(a) }
     books_on_hold.sort { |a, b| Recs.holds_compare(a, b) }
   end
 
-  STPAUL_BASE_URL = "https://alpha.stpaul.lib.mn.us"
-
-  def self.finds_either_title(book_part)
-    # <th class="patFuncBibTitle" scope="row"><a href="/record=b1266537~S16">
-    # <span class="patFuncTitleMain">Feed zone portables : a cookbook et cetera / Biju Thomas & Allen Lim.</span></a><br />
-    # </th>
-    title_span = book_part.css("th.patFuncBibTitle span.patFuncTitleMain").first
-    # remove the author after a /  remove possible sub title after a :
-    # remove the author after an old style title [by] author sequence
-    title = title_span.inner_text.split(" /")[0].split(" :")[0].split(" [by]")[0]
-    title = title[0, 42]
-    title.sub(/\.$/, "")
-  end
+  STPAUL_BASE_URL = "https://sppl.bibliocommons.com"
 
   def self.find_checkedout_title(book_part)
-    finds_either_title(book_part)
+    # <a><span class="title-content">Introducing Elixir</span>
+    #   <span class="sr-only ">Introducing Elixir, Book</span></a>
+    book_part.css("a span.title-content").first.inner_text
   end
 
   def self.find_checkedout_date(book_part)
-    # <td class="patFuncStatus"> DUE 10-18-16 </td>
-    td_status = book_part.css("td.patFuncStatus").first
-    status = td_status.inner_text.strip
-    date_match = status.match(
-      /^.* ([0-9]{2})-([0-9]{2})-([0-9]{2}).*$/
-    ).not_nil!
-  rescue e : NilAssertionError
-    Time.local(1, 1, 1)
-  else
-    Time.local("20#{date_match[3]}".to_i, date_match[1].to_i, date_match[2].to_i)
+    # <div class="cp-checked-out-due-on">
+    #   <span>Due by <span class="cp-short-formatted-date">Nov 19, 2018</span></span></div>
+    date_text =
+      book_part.css("div.cp-checked-out-due-on span.cp-short-formatted-date").first.inner_text
+    Time.parse(date_text, "%b %d, %Y", Time::Location.local)
   end
 
   def self.find_renewed_count(book_part)
-    # <td class="patFuncStatus">
-    #   DUE 11-01-16 <span class="patFuncRenewCount">Renewed 3 times</span></td>
-    # case where there is a nested span, not seen in find_checkedout_date example snippet
-    renewed_span = book_part.css("td.patFuncStatus span.patFuncRenewCount").first?
-    renewed_count_text = "Renewed 0 times"
-    if renewed_span
-      renewed_count_text = renewed_span.inner_text.strip
+    # old <div class="cp-renew-count">
+    # old   <span>Renewed</span> <span>1 time</span></div>
+    # now
+    # <div class="cp-renew-count">Renewed 2 times</div>
+    renewed_count_div = book_part.css("div.cp-renew-count").first? # Myhtml::Iterator.first? yields Myhtml::Node or Nil
+    if renewed_count_div
+      n_times_text = renewed_count_div.inner_text
+    else
+      n_times_text = "0 times"
     end
-    renewed_count_match = renewed_count_text.match(
-      /^Renewed ([0-9]{1}) times*$/ # *s for time or times possibly appearing
-    ).not_nil!
-    renewed_count_match[1].to_i
+    n_times_text.match(/(\d+) times*/).not_nil![1].to_i
+  end
+
+  def self.find_number_waiting(book_part)
+    # old <div class="cp-held-copies-count">
+    # old  <span>1 person waiting</span></div>
+    # now
+    # <div class="cp-held-copies-count">27 people waiting</div>
+    number_waiting_div = book_part.css("div.cp-held-copies-count").first? # Myhtml::Node or Nil
+    if number_waiting_div
+      n_people_waiting_text = number_waiting_div.inner_text
+    else
+      n_people_waiting_text = "0 people waiting"
+    end
+    n_people_waiting_text.match(/(\d+) (person|people) waiting/).not_nil![1].to_i
   end
 
   def self.find_on_hold_title(book_part)
-    finds_either_title(book_part)
+    # <a><span class="title-content">A History of America in Ten Strikes</span>
+    #   <span class="sr-only">A History of America in Ten Strikes, Book</span></a>
+    book_part.css("a span.title-content").first.inner_text
   end
 
-  def self.find_on_hold_date(status)
-    # status is e.g., "Ready. Must pick up by 10-25-16"
-    date_match = status.match(
-      /^.* ([0-9]{2})-([0-9]{2})-([0-9]{2}).*$/
-    ).not_nil!
-  rescue e : NilAssertionError
-    Time.local(1, 1, 1)
-  else
-    Time.local("20#{date_match[3]}".to_i, date_match[1].to_i, date_match[2].to_i)
+  def self.find_on_hold_date(book_part)
+    # <div class="holds-status ready_for_pickup">
+    #   <div class="cp-holds-secondary-info">
+    #     <span>Pick up by <span class="cp-short-formatted-date">Nov 14, 2018</span></span></div></div>
+    date_text = book_part.css("div.holds-status.ready_for_pickup span.cp-short-formatted-date").first.inner_text
+    Time.parse(date_text, "%b %d, %Y", Time::Location.local)
   end
 end
